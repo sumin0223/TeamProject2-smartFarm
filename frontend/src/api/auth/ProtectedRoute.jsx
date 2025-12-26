c651743d41f1038759529be6f52d62b7179f1839
eddy0417 <eddy0417@naver.com>
2025-12-27 00:08:44 +0900

    주석 한줄 추가

--- frontend/src/api/auth/ProtectedRoute.jsx
+++ frontend/src/api/auth/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
+// 로그인 여부 판단은 무조건 ProtectedRoute
 export default function ProtectedRoute({ children }) {
   const { user } = useAuth();
   const location = useLocation();
